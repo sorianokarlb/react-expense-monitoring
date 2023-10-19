39f7980935f73dc565d86335f574264343bb4f22
sorianokarlb <kbsoriano98@gmail.com>
2023-10-20 01:30:01 +0800

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Login from "./views/Login";
+import { Outlet } from "react-router-dom";
 
 function App() {
 
 
   return (
     <div>
-      <Login/>
+      <Outlet />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import {NextUIProvider} from "@nextui-org/react";
 import App from './App.jsx'
 import './assets/index.css'
+import Login from './views/Login.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<Login />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </React.StrictMode>,
 )
